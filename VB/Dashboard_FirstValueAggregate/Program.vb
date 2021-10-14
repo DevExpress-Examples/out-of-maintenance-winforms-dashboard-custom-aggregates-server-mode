ff7b1ee9552a5feb5f4b2d9bfb348014f38e4f9c
zakhodyaeva <margarita.zakhodyaeva@devexpress.com>
2021-10-14 13:10:32 +0300

    add VB

---
+++ VB/Dashboard_FirstValueAggregate/Program.vb
@@ -0,0 +1,24 @@
+﻿Imports DevExpress.Data.Filtering
+Imports DevExpress.LookAndFeel
+Imports DevExpress.Skins
+Imports DevExpress.UserSkins
+Imports System
+Imports System.Windows.Forms
+
+Namespace Dashboard_FirstValueAggregate
+	Friend Module Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread>
+		Sub Main()
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			BonusSkins.Register()
+			SkinManager.EnableFormSkins()
+			UserLookAndFeel.Default.SetSkinStyle("Office 2019 Colorful")
+			CriteriaOperator.RegisterCustomFunction(New FirstValueAggregateFunction())
+			Application.Run(New Form1())
+		End Sub
+	End Module
+End Namespace
