aa086936feb9f90ea160687a566f9cef7872d0c9
ritazakhodyaeva <57389482+ritazakhodyaeva@users.noreply.github.com>
2021-10-14 14:16:45 +0300

    Update Program.cs

--- CS/Dashboard_FirstValueAggregate/Program.cs
+++ CS/Dashboard_FirstValueAggregate/Program.cs
@@ -1,9 +1,9 @@
-﻿using DevExpress.Data.Filtering;
+using DevExpress.Data.Filtering;
+using System;
+using System.Windows.Forms;
 using DevExpress.LookAndFeel;
 using DevExpress.Skins;
 using DevExpress.UserSkins;
-using System;
-using System.Windows.Forms;
 
 namespace Dashboard_FirstValueAggregate
 {
